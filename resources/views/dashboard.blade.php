ab9ec29b35361daed8c7ab9d77950f362c4fb2f0
NandaDwiCahyo <nandad.cahyo@gmail.com>
2020-10-28 10:35:51 +0700

    Adding some table in dashboard menu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,23 @@
 @section('title', 'KlasKyta')
 
 @section('content')
-<h1>Dashboard</h1>
+<h1 class="text-center">Dashboard</h1>
+<table class="table">
+	<thead class="thead-dark">
+		<tr>
+			<th scope="col">#</th>
+			<th scope="col">Name</th>
+			<th scope="col">Contact</th>
+			<th scope="col">Address</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th scope="row">1</th>
+			<td>George</td>
+			<td>37283439</td>
+			<td>Golden Street 4</td>
+		</tr>
+	</tbody>
+</table>
 @endsection
